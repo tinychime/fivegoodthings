2b6f258106a9b251ce39efb54f3b4efed8aa720e
Josh Osborne <joshosbrn@gmail.com>
2016-06-05 22:56:23 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// fade a bunch of stuff out on scroll
+$(window).scroll(function(){
+  // $(".info").css("opacity", 1 - $(window).scrollTop() / 600);
+  $(".site-header").css("opacity", 1 - $(window).scrollTop() / 200);
+});
+
+// spin the header stuff a little on scroll
+$(window).scroll(function() {
+  var theta = $(window).scrollTop() / 1200 % Math.PI;
+  $('#red').css({ transform: 'rotate(' + theta + 'rad)' });
+  $('#blue').css({ transform: 'rotate(-' + theta + 'rad)' });
+});
+
+$(function() {
+    $('body').removeClass('fade-out');
+});
